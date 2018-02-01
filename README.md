a5f604fe926fadfdf85029a514609dd64012dd14
Evgeniy Kochetov <evgkoch@list.ru>
2018-02-01 23:16:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# gulp-adCombo
+npm i
+npm i gulp-cli
+npm install --seve-dev gulp@next
---
+++ gulpfile.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const gulp = require('gulp');
+
+// const gulpWebpack = require('gulp-webpack');
+// const webpack = require('webpack');
+// const webpackConfig = require('./webpack.config.js');
+const pug  = require('gulp-pug');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const groupMediaQueries = require('gulp-group-css-media-queries');//ищет одинаковые медиа условия и группирует их
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cssunit = require('gulp-css-unit');
+const replace = require('gulp-replace');
+const del = require('del');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync').create();
+
+const paths =  {
+  src: './src/',              // paths.src
+  build: './build/',          // paths.build
+    templates:{
+      pages:'src/templates/pages/*.pug',
+      src:'src/templates/**/*.pug',
+      dest:'./build'
+    },
+    images:{
+        src:'src/images/**/*.*',
+        dest:'./build/images/'
+    },
+    fonts:{
+      src:'src/fonts/**/*.*',
+        dest:'./build/fonts/'
+    },
+    scripts:{
+      src:'src/js/**/*.js',
+        dest:'./build/js/'
+    }
+};
+
+//pug
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({pretty:true}))
+        .pipe(gulp.dest(paths.build));
+}
+
+//scss
+function styles() {
+  return gulp.src(paths.src + 'scss/style.scss')
+    .pipe(plumber())// что бы если будет ошибка компиляции то процесс слежения не остонавливался
+    .pipe(sassGlob())
+    .pipe(sass()) // { outputStyle: 'compressed' }
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(cssunit({type: 'px-to-rem',rootSize: 16}))
+    .pipe(groupMediaQueries())//ищет одинаковые медиа условия и группирует их
+    .pipe(gulp.dest(paths.build + 'css/'))
+}
+
+// перекладывает  images
+function images() {
+    return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+};
+
+//fonts
+function fonts() {
+    return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest));
+}
+
+
+function scripts() {
+    return gulp.src(paths.src + 'js/main.js')
+        .pipe(plumber())
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest(paths.build + 'js/'))
+}
+
+//html
+function htmls() {
+  return gulp.src(paths.src + '*.html')
+    .pipe(plumber())
+    .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
+    .pipe(gulp.dest(paths.build));
+}
+
+
+//clean
+function clean() {
+  return del('build/*.html')
+}
+
+//watch
+function watch() {
+  gulp.watch(paths.src + 'scss/**/*.scss', styles);
+  gulp.watch(paths.src + 'js/*.js', scripts);
+  gulp.watch(paths.src + '*.html', htmls);
+  gulp.watch(paths.src + '**/*.pug', templates);
+  gulp.watch(paths.images.src,images);
+  gulp.watch(paths.fonts.src,fonts);
+}
+
+
+//browserSync
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: paths.build
+    }
+  });
+  browserSync.watch(paths.build + '**/*.*', browserSync.reload);
+}
+
+//
+exports.templates = templates;
+exports.styles = styles;
+exports.images = images;
+exports.fonts = fonts;
+exports.scripts = scripts;
+exports.htmls = htmls;
+exports.clean = clean;
+exports.watch = watch;
+
+//параметры выполнения
+gulp.task('build', gulp.series(
+  clean,
+  // styles,
+  // scripts,
+  // htmls
+  gulp.parallel(styles, htmls,templates,scripts)
+));
+
+//Запуск
+gulp.task('default', gulp.series(
+  clean,
+  gulp.parallel(styles, htmls,templates,images,fonts,scripts),
+  gulp.parallel(watch, serve)
+));
